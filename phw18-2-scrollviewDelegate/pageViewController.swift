2cca1c5cf9d04032d63d3990cb57d7d4ba4c07e3
jasonhung <hello@ifhavebugcallme.com>
2023-12-30 02:20:59 +0800

    ver 2023.12.30

--- phw18-2-scrollviewDelegate/pageViewController.swift
+++ phw18-2-scrollviewDelegate/pageViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class pageViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet var drinkImageViews: [UIImageView]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +20,22 @@ class pageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 定義 protocol UIScrollViewDelegate 的 function
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+            let page = scrollView.contentOffset.x / scrollView.bounds.width
+            pageControl.currentPage = Int(page)
     }
-    */
+    func viewForZooming(in scrollView: UIScrollView) -> UIView?{ // return a view that will be scaled. if delegate returns nil, nothing happens
+        //以Page Control的所在頁數判斷現在是哪一張圖片要進行縮放動作
+        return drinkImageViews[pageControl.currentPage]
+    }
 
+    @IBAction func changePage(_ sender: UIPageControl) {
+        let point = CGPoint(x: scrollView.bounds.width * CGFloat(sender.currentPage), y: 0)
+                scrollView.setContentOffset(point, animated: true)
+    }
+}
+
+extension pageViewController: UIScrollViewDelegate {
+    
 }
